273283d8d134f3df3cfb1de38624063b2df9b599
jllusty <jllusty@gmail.com>
2021-01-25 23:40:06 -0600

    Added Graphics, including a GLAD loader and using GLM.

---
+++ src/camera.h
@@ -0,0 +1,12 @@
+#ifndef CAMERA_H
+
+#include <glm/glm.hpp>
+
+struct Camera {
+    float x;
+    float y;
+    float z;
+};
+
+#define CAMERA_H
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,67 +3,113 @@
 // C++ Filesystem
 #include <filesystem>
 // C++ I/O Streams
-#include <iostream>
-// C++ Strings
-#include <string>
-// GLFW Header
-#include <GLFW/glfw3.h>
+#include <stdio.h>
+// assert
+#include <assert.h>
+// OpenGL
+#include "media/graphics.h"
+// OpenAL
+#include "media/audio.h"
+// Camera Object
+#include "camera.h"
+Camera camera;
 
-# include "media/audio.h"
+// Process Input Handle
+void processInput(GLFWwindow* window);
+
+// Frame-by-frame Timing
+float deltaTime{ 0.0f };
+float lastFrame{ 0.0f };
+
+// Highly abstract Mouse Object
+struct Mouse {
+    double x;
+    double y;
+};
+Mouse mouse;
+
+// Example Nodes Attributes
+// Node Positions
+glm::vec3 nodePositions[] = {
+    glm::vec3(0.0f, 0.0f, 0.0f),
+    glm::vec3(1.0f, 0.0f, 0.0f)
+};
+// Node Colors
+glm::vec3 nodeColors[] = {
+    glm::vec3(1.0f, 0.0f, 0.0f),
+    glm::vec3(0.0f, 1.0f, 0.0f)
+};
 
 int main(void)
 {
+    /* Initialize Graphics */
+    graphics::init();
+    // Create Shader Program
+    graphics::create_shader("../../src/media/shaders/vertex.glsl", "../../src/media/shaders/frag.glsl");
+    // Load Vertex Data (for Quad)
+    graphics::load_buffers();
+    // Load Texture Data (for Node)
+    graphics::load_texture("../../res/node.png");
+    
     /* Initialize OpenAL */
     audio::init();
     // add a sound
-    auto sound1 = audio::add_sound("../../res/sound.ogg");
-    auto sound2 = audio::add_sound("../../res/bell.ogg");
-
-    //  set up a sound source
-        /* GLFW window instance*/
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return EXIT_FAILURE;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Opinion Dynamics", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return EXIT_FAILURE;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-    glClearColor(0.4f, 0.3f, 0.4f, 0.0f);
+    ALuint sound1 = audio::add_sound("../../res/sound.ogg");
+    // add a source
+    ALuint* pSource1 = audio::create_source();
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(graphics::window))
     {
+        // per-frame timing
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        /* Process Input */
+        processInput(graphics::window);
+        // get cursor position
+        glfwGetCursorPos(graphics::window, &mouse.x, &mouse.y);
+        // use it to set the pitch (lel)
+        audio::set_source(pSource1, (float)mouse.x/640.f);
         // if space is pressed, play sound
-        if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
-            audio::play_sound(sound1);
+        if (glfwGetKey(graphics::window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+            audio::play_sound(pSource1, sound1);
         }
-        else if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS) {
-            audio::play_sound(sound2);
-        }
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Render Nodes */
+        graphics::render();
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
+        glfwSwapBuffers(graphics::window);
         /* Poll for and process events */
         glfwPollEvents();
     }
 
-    /* GLFW clean-up */
-    glfwTerminate();
-
-    /* OpenAL clean-up */
+    /* OpenAL: clean-up */
+    audio::destroy_source(pSource1);
     audio::destroy();
 
+    /* Graphics: clean-up */
+    graphics::cleanup();
+
     return EXIT_SUCCESS;
+}
+
+void processInput(GLFWwindow *window) {
+    const float speed = 2.5f * deltaTime;
+    // pan
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.y += speed;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.x -= speed;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.y -= speed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.x += speed;
+    // zoom
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+        camera.z -= speed;
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+        camera.z += speed;
 }
